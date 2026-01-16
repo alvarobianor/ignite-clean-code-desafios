4c39a7f2871427e5b0c997b8924a90d15f838d85
Álvaro Bianor <bianor.alvaro12@gmail.com>
2026-01-16 17:39:03 -0300

    refactor: improve code style, update date format in return, and log function output.

--- desafios/09-syntatic-sugars.ts
+++ desafios/09-syntatic-sugars.ts
@@ -1,26 +1,30 @@
 function getFirstFiveRatings(ratings) {
-  return ratings.length >= 5 && ratings.slice(0, 5)
+  const hasLenghtNecessary = ratings.length >= 5;
+
+  return hasLenghtNecessary && ratings.slice(0, 5);
 }
 
 function sumFirstFiveRatings(ratings) {
-  const ratingsBool = !!ratings
+  const ratingsBool = !!ratings;
 
   if (ratingsBool) {
-    const firstFiveRatings = getFirstFiveRatings(ratings)
+    const firstFiveRatings = getFirstFiveRatings(ratings);
 
-    if (!firstFiveRatings) return { error: 'there must be at least 5 ratings' }
+    if (!firstFiveRatings) return { error: "there must be at least 5 ratings" };
 
     let ratingsSum = 0;
 
     for (const rating of firstFiveRatings) {
-      ratingsSum += +rating
+      ratingsSum += +rating;
     }
 
-    return { ratingsSum, created_at: +new Date() }
+    return { ratingsSum, created_at: new Date().toDateString() };
   }
 
-  return { error: 'ratings is required' }
+  return { error: "ratings is required" };
 }
 
-const appRatings = ['5', '3', '4', '4', '5', '1', '5', '4', '4', '3']
-sumFirstFiveRatings(appRatings)
+const appRatings = ["5", "3", "4", "4", "5", "1", "5", "4", "4", "3"];
+const result = sumFirstFiveRatings(appRatings);
+
+console.log(result);
